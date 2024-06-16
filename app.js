58a8535ce5292511cebc3aa49031536f5e99666b
RandomBroLol <chrisjudah112@gmail.com>
2024-06-16 21:27:59 +0200

    Updated index.html, info.html, and downloads.html with top bar and dark mode functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,18 +12,26 @@ app.get('/alert', (req, res) => {
     res.json({ alertMessage });
 });
 
+// Serve index.html for the root path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/info.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'info.html'));
+// Middleware to handle all other HTML requests
+app.use((req, res, next) => {
+    const filePath = path.join(__dirname, 'public', req.path);
+    fs.stat(filePath, (err, stats) => {
+        if (err || !stats.isFile()) {
+            // File doesn't exist or not a regular file (e.g., directory)
+            res.status(404).redirect('/404.html');
+        } else {
+            // Serve the requested HTML file
+            res.sendFile(filePath);
+        }
+    });
 });
 
-app.get('/secret.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'secret.html'));
-});
-
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
